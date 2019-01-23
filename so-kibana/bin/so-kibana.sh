5d961b26b2837934e35c31ffa5168a6814e7259d
Mike Reeves <luke@geekempire.com>
2019-01-23 16:02:00 -0500

    Kibana Docker - Import New Dashboards

--- so-kibana/bin/so-kibana.sh
+++ so-kibana/bin/so-kibana.sh
@@ -4,7 +4,6 @@
 
 KIBANA_VERSION=6.5.2
 MAX_WAIT=60
-MASTERSERVER=$MASTERSERVER
 
 # Check to see if Kibana is available
 wait_step=0
@@ -54,7 +53,7 @@ echo
 # Apply all the dashboards
 # Load dashboards, visualizations, index pattern(s), etc.
 for i in /usr/share/kibana/dashboards/*.json; do
-  sed -i "s/OSQPLACEHOLDER/$MASTERSERVER/g" $i
+  sed -i "s/OSQPLACEHOLDER/$MASTER/g" $i
 	curl -XPOST localhost:5601/api/kibana/dashboards/import?force=true -H 'kbn-xsrf:true' -H 'Content-type:application/json' -d @$i >> /var/log/kibana/dashboards.log 2>&1 &
 	echo -n "."
 done
