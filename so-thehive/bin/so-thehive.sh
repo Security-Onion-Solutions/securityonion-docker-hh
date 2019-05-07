c39e9045802edaa72f10d15e4dee460ab55815b7
Mike Reeves <luke@geekempire.com>
2019-05-07 15:43:22 -0400

    Hive Docker - Fix entrypoint

---
+++ so-thehive/bin/so-thehive.sh
@@ -0,0 +1,11 @@
+rm -f garbage_file
+while ! wget -O garbage_file ${ELASTICSEARCH_HOST}:9500 2>/dev/null
+do
+	echo "Waiting for Elasticsearch..."
+	rm -f garbage_file
+	sleep 1
+done
+rm -f garbage_file
+sleep 5
+
+/opt/thehive/bin/thehive
