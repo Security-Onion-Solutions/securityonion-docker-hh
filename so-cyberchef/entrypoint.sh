37194d3dd9544d680666d05b98c6c22bd8466e14
Mike Reeves <mike.reeves@securityonionsolutions.com>
2020-03-17 10:30:40 -0400

    Update Versions

--- so-cyberchef/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Make data available to the host
-mv /CyberChef/build/prod/assets /prod
-mv /CyberChef/build/prod/imagees /prod
-mv /CyberChef/build/prod/modules /prod
-mv /CyberChef/build/prod/index.html /prod
-/usr/bin/bash
