19918f9f0882d9cd011a8bb6fde7011844b8c6c5
William Wernert <william.wernert@securityonionsolutions.com>
2020-03-18 09:28:06 -0400

    Remove so-auth from repo
    SO-Auth can be built from its own repo now so remove it from this one

--- so-auth/api/api/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-DB = SQLAlchemy()
--- so-auth/ui/src/plugins/ant-design-vue.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue';
-import Antd from 'ant-design-vue';
-import '../antd-variables.less';
-
-Vue.use(Antd);
